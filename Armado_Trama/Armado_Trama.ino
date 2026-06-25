fe569c7b582fce753a7c04ab90b6b618bf6fccdc
Paulina <49317047@est.ort.edu.ar>
2026-06-25 10:21:13 -0300

    Actualización del código de prueba: ahora incluye una máquina de estados y una lista de espera.
    Verificar si funciona correctamente

--- Armado_Trama/Armado_Trama.ino
+++ Armado_Trama/Armado_Trama.ino
@@ -1,38 +1,110 @@
-// Código de prueba armado con I.A.
+// Código de prueba hecho con IA, ahora implementando la máquina de estados.
 
 #include "Dispositivo.h"
+#include <vector>  // implenentación de la librería Vector para la lista de espera
 
-#define BAUD 115200
+#define ERROR % // definir el caracter que representa error.
+#define BAUD 115200 // definición de BAUD
 
-// Función que recibe el objeto y retorna el String con la trama
-String armarTrama(const dispositivo &d) {
-    String trama = "#";
-    trama += d.getApodoDispositivo() + ",";
-    trama += String(d.getEmergencia()) + ","; // Convertimos el bool a "1" o "0"
-    trama += d.getApodoSensor1() + ",";
-    trama += d.getApodoSensor2() + ",";
-    trama += d.getApodoSensor3() + ",";
-    trama += d.getResultadoSensor1() + ",";
-    trama += d.getResultadoSensor2() + ",";
-    trama += d.getResultadoSensor3() + "*";
-    
-    return trama;
-}
+// Definición de estados
+typedef enum {
+  DISPOSITIVO,
+  EMG,
+  SENSOR1,
+  SENSOR2,
+  SENSOR3,
+  VALOR1,
+  VALOR2,
+  VALOR3,
+  ARMADO_ENVIADO  // Estado final para transmitir y limpiar
+} ESTADOSARMADO_t;
+
+ESTADOSARMADO_t estadoArmado = DISPOSITIVO;  // Estado inicial
+String tramaActual = "";                     // Aquí se irá armando la trama paso a paso
+std::vector<dispositivo> filaDispositivos;   // Nuestra fila de objetos
+
+
 
 void setup() {
-    Serial.begin(BAUD);
+  Serial.begin(BAUD);
 
-    // Creamos un objeto de prueba
-    dispositivo miDispositivo("P1", 0, "S1", "S2", "S3", "-", "-", "-");
-
-    // Llamamos a la función pasándole nuestro objeto
-    String resultado = armarTrama(miDispositivo);
-
-    // Imprimimos el resultado en el monitor serie
-    Serial.println(resultado); 
-    // Imprimirá: #Disp01,1,Temp,Hum,Pres,25C,60%,1013hPa
+  // Objetos de prueba
+  filaDispositivos.push_back(dispositivo("Disp_A", false, "Temp", "Hum", "Pres", "22", "50", "1012"));
+  filaDispositivos.push_back(dispositivo("Disp_B", true, "Volt", "Curr", "Freq", "5", "2", "60"));  // Con emergencia
+  filaDispositivos.push_back(dispositivo("Disp_C", false, "Lux", "UV", "Irrad", "300", "1", "15"));
 }
 
 void loop() {
-    // Vacío por ahora
+  //if (armarTrama != ERROR) {
+    String resultado = armarTrama(filaDispositivos);
+    Serial.println("Se guardó la trama exitosamente");
+    Serial.println(resultado);
+  //} else {
+   // Serial.println("error");
+  //}
+}
+
+String armarTrama(std::vector<dispositivo> &fila) {
+    switch (estadoArmado) {
+        
+        case DISPOSITIVO:
+            // Si la fila está vacía, no hacemos nada y salimos del switch
+            if (filaDispositivos.empty()) {
+                break; 
+            }
+            // Si hay alguien en la fila, empezamos a armar su trama
+            tramaActual = "#" + filaDispositivos.front().getApodoDispositivo() + ",";
+            estadoArmado = EMG; // Siguiente estado
+            break;
+
+        case EMG:
+            // Añadimos la emergencia a la trama
+            tramaActual += String(filaDispositivos.front().getEmergencia()) + ",";
+            estadoArmado = SENSOR1;
+            break;
+
+        case SENSOR1:
+            tramaActual += filaDispositivos.front().getApodoSensor1() + ",";
+            estadoArmado = SENSOR2;
+            break;
+
+        case SENSOR2:
+            tramaActual += filaDispositivos.front().getApodoSensor2() + ",";
+            estadoArmado = SENSOR3;
+            break;
+
+        case SENSOR3:
+            tramaActual += filaDispositivos.front().getApodoSensor3() + ",";
+            estadoArmado = VALOR1;
+            break;
+
+        case VALOR1:
+            tramaActual += filaDispositivos.front().getResultadoSensor1() + ",";
+            estadoArmado = VALOR2;
+            break;
+
+        case VALOR2:
+            tramaActual += filaDispositivos.front().getResultadoSensor2() + ",";
+            estadoArmado = VALOR3;
+            break;
+
+        case VALOR3:
+            // El último valor no lleva coma al final
+            tramaActual += filaDispositivos.front().getResultadoSensor3();
+            estadoArmado = ARMADO_ENVIADO;
+            break;
+
+        case ARMADO_ENVIADO:
+            // Aquí la trama está lista. La enviamos (por ejemplo, por Serial)
+            Serial.print("Trama Lista y Enviada: ");
+            Serial.println(tramaActual);
+
+            // IMPORTANTE: Sacamos al dispositivo procesado de la fila
+            filaDispositivos.erase(filaDispositivos.begin());
+
+            // Reseteamos las variables para el siguiente dispositivo de la fila
+            tramaActual = "";
+            estadoArmado = DISPOSITIVO;
+            break;
+    }
 }
